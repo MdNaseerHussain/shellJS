88199247e1afdb50895aee536204087dd74f8b3c
MdNaseerHussain <mdnajju013@gmail.com>
2022-12-03 23:30:24 +0530

    Recursion removed

--- shell.js
+++ shell.js
@@ -4,48 +4,43 @@ const os = require('os');
 const { exec } = require('child_process');
 
 currentDirectory = os.homedir();
-console.log(currentDirectory)
 
-const newCommand = () => {
-    const command = readline.question(`shell ${currentDirectory} $ `);
-
-    const execPromise = (command) => {
-        return new Promise((resolve, reject) => {
-            exec(`cd ${currentDirectory}; ${command}`, {shell: 'powershell'}, (error, stdout, stderr) => {
-                if (error) {
-                    reject(`error: ${error.message}`);
-                } else if (stderr) {
-                    reject(`stderr: ${stderr}`);
-                } else {
-                    resolve(`${stdout}`);
-                }
-            });
+const execPromise = async (command) => {
+    return new Promise((resolve, reject) => {
+        exec(`cd ${currentDirectory}; ${command}`, {shell: 'powershell'}, (error, stdout, stderr) => {
+            if (error) {
+                reject(`error: ${error.message}`);
+            } else if (stderr) {
+                reject(`stderr: ${stderr}`);
+            } else {
+                resolve(`${stdout}`);
+            }
         });
-    };
+    });
+}
+
+const newCommand = async () => {
+    const command = readline.question(`shell ${currentDirectory} $ `);
+    let result;
 
     switch (command.trim().split(' ')[0]) {
         case 'exit':
             process.exit();
         case 'cd':
             const newDirectory = path.resolve(currentDirectory, command.trim().split(' ')[1]);
-            execPromise(`cd ${newDirectory}`).then((_) => {
-                currentDirectory = newDirectory;
-                newCommand();
-            }).catch((error) => {
-                console.log(error);
-                newCommand();
-            });
+            result = await execPromise(`cd ${newDirectory}`);
+            currentDirectory = newDirectory;
             break;
         default:
-            execPromise(command).then((stdout) => {
-                console.log(stdout);
-                newCommand();
-            })
-            .catch((error) => {
-                console.log(error);
-                newCommand();
-            });
+            result = await execPromise(command);
+            console.log(result);
     }
 };
 
-newCommand();
+const main = async () => {
+    while (true) {
+        await newCommand();
+    }
+};
+
+main();
