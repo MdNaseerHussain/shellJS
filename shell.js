51f66914ac9354bf88fa517cb656b18a3f782245
MdNaseerHussain <mdnajju013@gmail.com>
2022-12-03 18:13:20 +0530

    pwd ls cd tested

--- shell.js
+++ shell.js
@@ -1,26 +1,51 @@
 const readline = require('readline-sync');
 const path = require('path');
 const os = require('os');
+const { exec } = require('child_process');
 
 currentDirectory = os.homedir();
+console.log(currentDirectory)
 
-while(true) {
+const newCommand = () => {
     const command = readline.question(`shell ${currentDirectory} $ `);
 
-    switch (command) {
-        case 'pwd':
-            console.log(currentDirectory);
-            break;
-        case 'ls':
-            //
-            break;
-        case 'cd':
-            //
-            break;
+    const execPromise = (command) => {
+        return new Promise((resolve, reject) => {
+            exec(`cd ${currentDirectory}; ${command}`, {shell: 'powershell'}, (error, stdout, stderr) => {
+                if (error) {
+                    reject(`error: ${error.message}`);
+                } else if (stderr) {
+                    reject(`stderr: ${stderr}`);
+                } else {
+                    resolve(`${stdout}`);
+                }
+            });
+        });
+    };
+
+    switch (command.trim().split(' ')[0]) {
         case 'exit':
             process.exit();
-        default:
-            console.log('Command not found');
+        case 'cd':
+            const newDirectory = path.resolve(currentDirectory, command.trim().split(' ')[1]);
+            execPromise(`cd ${newDirectory}`).then((_) => {
+                currentDirectory = newDirectory;
+                newCommand();
+            }).catch((error) => {
+                console.log(error);
+                newCommand();
+            });
             break;
+        default:
+            execPromise(command).then((stdout) => {
+                console.log(stdout);
+                newCommand();
+            })
+            .catch((error) => {
+                console.log(error);
+                newCommand();
+            });
     }
-}
+};
+
+newCommand();
